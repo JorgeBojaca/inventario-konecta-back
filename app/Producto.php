2aa19612f1913d2f95e81d5710d302e56088d1c1
JorgeBojaca <jorgebojaca@gmail.com>
2020-06-06 03:19:51 -0500

    Creación de método 'makeSale' encargado de realizar la venta y disminuir el stock si hay productos disponibles

--- app/Producto.php
+++ app/Producto.php
@@ -9,4 +9,20 @@ class Producto extends Model
     public $timestamps = false;
 
     protected $guarded = [];
+
+    public static function makeSale($id)
+    {
+        $producto = self::find($id);
+        if($producto->stock > 0){
+            $producto->stock -= 1;
+            $producto->fecha_ultima_venta = date('Y-m-d H:i:s');
+            if($producto->save()){
+                return response()->json($producto->fecha_ultima_venta);
+            }else{
+                response("Error")->setStatusCode(422);
+            }
+        }else {
+            return response("No venta por no tener producto en stock")->setStatusCode(422);;
+        }
+    }
 }
